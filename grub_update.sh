a17b522b03d04f279755105ffd47569ab6f925f7
Vyacheslav K <pdxrico@gmail.com>
2023-02-26 12:05:56 +0300

    Add script

---
+++ grub_update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Greeting
+echo "Welcome to the GRUB update script!"
+
+# Backup GRUB configuration
+echo "Backing up GRUB configuration..."
+cp /etc/default/grub /etc/default/grub.bak
+
+# Inserting a string into GRUB
+echo "Inserting string into GRUB..."
+echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash new_string\"" >> /etc/default/grub
+
+# Update GRUB
+echo "Updating GRUB..."
+update-grub
+
+# Countdown until reboot
+echo "Rebooting in 10 seconds..."
+for i in {10..1}; do
+    echo "$i..."
+    sleep 1
+done
+
+# # Reboot system
+# echo "Rebooting now!"
+# shutdown now -r
