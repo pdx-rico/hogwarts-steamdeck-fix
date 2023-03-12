55bc4abc68f7332b76722b4936b505c3809ca609
Vyacheslav K <pdx_rico@mail.ru>
2023-03-12 11:33:58 +0300

    update sleep

--- grub_update.sh
+++ grub_update.sh
@@ -3,26 +3,29 @@
 # Check if the new value is already present in GRUB_CMDLINE_LINUX_DEFAULT
 if grep -q "clearcpuid=514" /etc/default/grub; then
   echo "The fix is already present in GRUB_CMDLINE_LINUX_DEFAULT"
+  sleep 3
 else
   # Backup the original file
-  for i in {2..1}; do
-    sleep 1
-  done
-
+  echo "Backup grub..."
+  sleep 1
   cp /etc/default/grub /etc/default/grub.bak
   
   # Add the new value to GRUB_CMDLINE_LINUX_DEFAULT
-  for i in {2..1}; do
-    sleep 1
-  done
-
+  echo "Adding parameter in grub..."
+  sleep 1
   sed -i 's/^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 clearcpuid=514"/' /etc/default/grub
   
   # Update the GRUB configuration file
+  echo "Update grub..."
+  sleep 1
   update-grub
   
   # Reboot after a countdown
-  echo "Rebooting in 10 seconds..."
-  sleep 5
-  shutdown now -r
+  for i in {5..1}; do
+    echo "Reboot in $i seconds..."
+    sleep 1
+  done
+
+echo "Reboot!"
+shutdown now -r
 fi
